1d5ec4c54fd35017bff110c19e48883d7d132ce2
Sara Baqla <missatrox44@gmail.com>
2023-10-01 06:45:12 -0500

    created EventItem and EventList components, not styled, rendering on starting page

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 function EventItem(props) {
+  const { title, image, date, location, id } = props;
+
+  // transform date
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+// transform address
+const formattedAddress = location.replace(',', '\n');
+
+const exploreLink = `/events/${id}`;
 
   return (
     <li>
-      
+      <img src={'/' + image} alt={title} />
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            <time>{formattedDate}</time>
+          </div>
+          <div>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
     </li>
-  )
+  );
 }
 
-export default EventItem
+export default EventItem;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/EventList';
 
 function StartingPage() {
   // get all featured events
-  const featuredEvents = getFeaturedEvents;
+  const featuredEvents = getFeaturedEvents();
 
   return (
     <div>
-      
+      <EventList items={featuredEvents} />
     </div>
   );
 }
