301a9905d3a78b0536a758d83054a44bb21bcbb4
Sara Baqla <missatrox44@gmail.com>
2023-10-03 07:46:34 -0500

    added eventDetail directory with related components and styles

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+import {getEventById} from '../../dummy-data';
 
 function EventDetailPage() {
+
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById();
+
+  if (!event) {
+    return <p>No Event Found!</p>
+  }
+
   return (
     <div>
       <h1>Event Detail Page</h1>
